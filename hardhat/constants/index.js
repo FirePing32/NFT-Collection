2e8f8be91521f22d457d78bdd5fc484f3fe623cc
Prakhar Gurunani <prakhargurunani@gmail.com>
2022-01-28 09:28:48 +0530

    nft collection

---
+++ hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xf7eCD01B6496522665d0a1e28676A816558a904A";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-prakhargurunani.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
